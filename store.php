1eac7b78fcf1b0d9562dd58518ef744b01c3c38d
thyriaen <thyriaen@googlemail.com>
2018-11-28 02:11:22 +0100

    blublublu

---
+++ store.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Store</title>
+	</head>
+	<body style="background-color:black;  color: #ffffff;">
+		<center>
+	<body>
+		<br><br><br>
+		<H1>Thanks !</H1>
+		<?php
+
+			echo "Hello World!";
+			
+		?>
+	</body>
+</html>
